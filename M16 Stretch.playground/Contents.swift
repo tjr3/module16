2a0325db25d3993bd3b5d1d51927b7383579da33
Tyler Robinson <tyler.robinson.3@gmail.com>
2016-05-30 09:33:38 -0600

    stretch

---
+++ M16 Stretch.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+func getChange(change: Double) -> String {
+    let quarter = 0.25
+    let dime = 0.10
+    let nickle = 0.05
+    let penny = 0.01
+    
+    if x = 1.66 {
+        return ("6 quarters, 1 nickle, 1 dime, 1 penny")
+    }
+    
+}
+
+
+
+func changeType(amount: Double) -> String {
+    for i in 1...100 {
+        if  i = 0.25 % ==0 {
+        return ("Quarter")
+        }
+        if i = 0.10 % ==0 {
+        return ("Dime")
+        }
+        if i = 0.05 % ==0 {
+        return ("Nickle")
+        }
+        if i = 0.01 % ==0 {
+        return ("Penny")
+        }
+    }
+    
+}
